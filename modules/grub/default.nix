6e22ae04a9f5be99160ebd93ba12e1ed625717d5
muchstarlight <muchstarlight@163.com>
2024-12-28 22:47:46 +0800

    test fix driver

--- modules/grub/default.nix
+++ modules/grub/default.nix
@@ -19,4 +19,12 @@
       efiSysMountPoint = "/boot";
     };
   };
+
+  boot.kernelParams = [
+    "loglevel=7"
+    # Optional: To ignore the log level setting and always print all messages
+    "ignore_loglevel"
+    "modeset=1"
+  ];
+
 }
